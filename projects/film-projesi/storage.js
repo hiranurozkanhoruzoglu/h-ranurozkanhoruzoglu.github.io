28e4dbb43daa8263e8d59e2eaad0809775f28277
hiranurozkanhoruzoglu <79055283+hiranurozkanhoruzoglu@users.noreply.github.com>
2022-03-29 16:37:00 +0300

    film projesi yenileme

--- projects/film-projesi/storage.js
+++ projects/film-projesi/storage.js
@@ -1,34 +1,33 @@
-function saveToLocalStorage(newFilm) {
-    let filmler = JSON.parse(localStorage.getItem('Filmler'));
-    if (filmler === null) {
-        filmler = [];
+class Storage{
+    static addFilmToStorage(newFilm){
+      let films = this.getFilmsFromStorage();
+      films.push(newFilm);
+      localStorage.setItem("films",JSON.stringify(films))
     }
-    filmler.push(newFilm);
-    localStorage.setItem('Filmler', JSON.stringify(filmler));
-
-}
-
-function getLocalStorage() {
-    let film;
-    film = JSON.parse(localStorage.getItem("Filmler"));
-    return film;
-}
-
-function deleteFromStorage(deletefilm) {
-    let films = getLocalStorage();
-    films.forEach(function (film, index) {
-        if (film.title === deletefilm) {
-            films.splice(index, 1);
+    
+    static getFilmsFromStorage(){
+      let films;
+      if (localStorage.getItem("films")===null){
+        films = [];
+      }
+      else{
+        films = JSON.parse(localStorage.getItem("films"));
+      }
+      return films;
+    }
+    
+    static deleteFilmFromStorage(filmTitle){
+      let films = this.getFilmsFromStorage();
+      films.forEach(function(film,index){
+        if(film.title === filmTitle){
+          films.splice(index,1);
         }
-    });
-    localStorage.setItem("Filmler", JSON.stringify(films));
-}
-//   function deleteTodoFromStorage(deleteTodo){
-//     let todos = getTodosFromStorage();
-//     todos.forEach(function(todo,index){
-//       if(todo === deleteTodo){
-//         todos.splice(index,1);
-//       }
-//     });
-//     localStorage.setItem("todos",JSON.stringify(todos));
-//   }
+      });
+      localStorage.setItem("films",JSON.stringify(films));
+    }
+    
+    static clearAllFilmsFromStorage(){
+      localStorage.removeItem("films");
+    }
+  }
+  
--- projects/film-projesi/ui.js
+++ projects/film-projesi/ui.js
@@ -1,4 +1,4 @@
-class UI {
+class UI{
   static addFilmToUI(newFilm){
     // <tr>
     //   <td><img src="" class="img-fluid img-thumbnail"></td>
@@ -16,20 +16,42 @@ class UI {
        <td class="w-25"><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
      </tr>`
   }
-  static clearInputs(element1, element2, element3) {
+  static clearInputs(element1,element2,element3){
     element1.value = "";
     element2.value = "";
     element3.value = "";
   }
-  static displayMessage(message, type) {
+  static displayMessage(message,type){
     const cardBody = document.querySelector(".card-body");
     // Alert divini oluşturma
     const div = document.createElement("div");
-    div.className = `alert alert-${type}`;
+    div.className=`alert alert-${type}`;
     div.textContent = message;
     cardBody.appendChild(div);
-    setTimeout(function () {
+    setTimeout(function(){
       div.remove();
-    }, 2000);
+    },2000);
+  }
+  static deleteFilmFromUI(e){
+    e.parentElement.parentElement.remove();
+  }
+  static clearAllFilmsFromUI(){
+    const filmList = document.getElementById("films");
+    while(filmList.firstElementChild !== null){
+      filmList.firstElementChild.remove();
+    }
+  }
+  static loadAllFilms(films){
+    const filmList = document.getElementById("films");
+    films.forEach(function(film){
+      filmList.innerHTML += `
+     <tr>
+       <td class="w-25"><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+       <td class="w-25">${film.title}</td>
+       <td class="w-25">${film.director}</td>
+       <td class="w-25"><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+     </tr>`
+    })
   }
 }
+ 
